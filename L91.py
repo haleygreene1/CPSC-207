c2f47909ac3cbedba326a144c74eacd77613876b
haleygreene1 <116827080+haleygreene1@users.noreply.github.com>
2022-10-27 16:05:37 -0400

    Add files via upload

---
+++ L91.py
@@ -0,0 +1,7 @@
+def breakify (string):
+    new="<br>".join(string)
+    print(new)
+
+lines=["Haiku frogs in snow", "A limerick came from Nantucket", "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+breakify(lines)
---
+++ L92.py
@@ -0,0 +1,13 @@
+def remove_substring(string,sub):
+    output= []
+    index=0
+    while index < len(string):
+        if string[index:index+len(sub)]== sub :
+            index += len(sub)
+        else:
+            output.append(string[index])
+            index += 1
+    print("".join(output))
+
+
+remove_substring("Hello, test this is a test to see if the string removes test", "test")
