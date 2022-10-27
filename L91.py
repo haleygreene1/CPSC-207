fe2dcd535ef5dfa8e57642b32d051a353ec899ae
haleygreene1 <116827080+haleygreene1@users.noreply.github.com>
2022-10-27 15:57:23 -0400

    Add files via upload

---
+++ L91.py
@@ -0,0 +1,9 @@
+#Haley and Anna
+
+def breakify (string):
+    new="<br>".join(string)
+    print(new)
+
+lines=["Haiku frogs in snow", "A limerick came from Nantucket", "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+breakify(lines)
