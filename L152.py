76e2ac2e5331a3132ff0dbd34b509440c016dd6e
haleygreene1 <116827080+haleygreene1@users.noreply.github.com>
2022-12-08 15:59:55 -0500

    Add files via upload

---
+++ L152.py
@@ -0,0 +1,9 @@
+class RightRect:
+    def __init__(self,height,length):
+        self.height = height
+        self.length = length
+
+rr1 = RightRect(15,20)
+
+print(rr1.height)
+print(rr1.length)
---
+++ L154.py
@@ -0,0 +1,11 @@
+class Fabric:
+    def __init__(self,countryOfOrigin,color):
+        self.countryOfOrigin= countryOfOrigin
+        self.color = color
+
+def __str__(self):
+    return self.countryOfOrigin + "("+ str(self.color)+")"
+
+f1 = Fabric("Australia","Red")
+
+print(f1)
